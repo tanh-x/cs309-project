665bc2e90334f9d88eafdacbccd4f7c5d474a438
Thanh <kewber0@gmail.com>
2023-08-30 10:15:37 -0500

    Import backend intialzr project

---
+++ Backend/src/test/kotlin/com/kewargs/cs309/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kewargs.cs309
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
