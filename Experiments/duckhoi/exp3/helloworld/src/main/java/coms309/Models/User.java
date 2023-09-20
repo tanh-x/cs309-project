5d1cfabeff5baba0e709b75b624e0b7c500f1b9d
Khoi <hai.pham@sea-solution.com>
2023-09-20 14:04:13 -0500

    save progress

--- Experiments/duckhoi/exp3/helloworld/src/main/java/coms309/Models/User.java
+++ Experiments/duckhoi/exp3/helloworld/src/main/java/coms309/Models/User.java
@@ -1,2 +1,40 @@
-package coms309.Models;public class User {
+package coms309.Models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Getter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID ID;
+    @Setter
+    private String firstName;
+    @Setter
+    private String lastName;
+    @Setter
+    private String email;
+    @Setter
+    private String userName;
+    @Setter
+    private String address;
+
+    public User(String firstName, String lastName, String email, String userName, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.address = address;
+    }
+
+    public User() {
+
+    }
 }
