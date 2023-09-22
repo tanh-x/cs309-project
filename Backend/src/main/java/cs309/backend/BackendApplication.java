8e6210b7e83d7007747eaaef490384bf4d7728e2
Thanh <kewber0@gmail.com>
2023-09-22 09:59:56 -0500

    Save progress

--- Backend/src/main/java/cs309/backend/BackendApplication.java
+++ Backend/src/main/java/cs309/backend/BackendApplication.java
@@ -2,14 +2,8 @@ package cs309.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@EnableJpaRepositories
-//@ComponentScan("cs309.backend.jpa")
-//@EntityScan("cs309.backend.jpa")
 public class BackendApplication {
     public static void main(String[] args) { SpringApplication.run(BackendApplication.class, args); }
 
