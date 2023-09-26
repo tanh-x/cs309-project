e265db7cb64c843278c39c353db138df6ff15b21
Khoi <hai.pham@sea-solution.com>
2023-09-26 11:11:33 -0500

    AddJWTDependencies_and_EnableRepo

--- Backend/src/main/java/cs309/backend/BackendApplication.java
+++ Backend/src/main/java/cs309/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package cs309.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "cs309.backend.repo")
 public class BackendApplication {
     public static void main(String[] args) { SpringApplication.run(BackendApplication.class, args); }
 
