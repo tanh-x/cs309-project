fa18e42d055d5e072a1e4695eafe613430dfb580
Thanh <kewber0@gmail.com>
2023-10-06 11:44:56 -0500

    resolve jpa table naming

--- Backend/src/main/java/cs309/backend/controllers/StudentController.java
+++ Backend/src/main/java/cs309/backend/controllers/StudentController.java
@@ -1,10 +1,36 @@
 package cs309.backend.controllers;
 
+import cs309.backend.jpa.entity.user.StudentEntity;
+import cs309.backend.models.StudentData;
+import cs309.backend.services.StudentService;
+import jakarta.annotation.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.internalServerError;
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
-    private final
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("id/{id}")
+    public ResponseEntity<StudentData> getStudentById(@PathVariable int id) {
+        try {
+            StudentEntity student = studentService.getStudentByUid(id);
+            return ok(StudentData.fromEntity(student));
+        } catch (Exception e) {
+            return internalServerError().build();
+        }
+    }
 }
