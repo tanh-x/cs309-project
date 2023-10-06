6af91891216d9e4751dccda1310bb8c10565ba1e
Nhat Bui <nbui@iastate.edu>
2023-10-06 10:35:58 -0500

    blabla

--- Frontend/app/src/main/java/com/example/as1/request_user_api.java
+++ Frontend/app/src/main/java/com/example/as1/request_user_api.java
@@ -8,11 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,9 +26,10 @@ public class request_user_api extends AppCompatActivity {
     private Button req,back ;
     private TextView res;
 
-    private static final String URL_STRING_REQ = "coms-309-029.class.las.iastate.edu/api/user/username/Khshcua";
-    //public static final String URL_STRING_REQ = "https://bba21c77-fb12-4f95-abe7-3c93c07fa9f9.mock.pstmn.io/stud";
-    //public static final String URL_STRING_REQ = "http://10.0.2.2:8080/users/1";
+    private static final String URL_STRING_REQ = "http://cs309.kewargs.com:8080/api/user/username/student2";//127.0.0.1:8080/api/user/login
+    //private static final String URL_STRING_REQ = "http://cs309.kewargs.com:8080/api/user/get-test-data/100";
+
+//    public static final String URL_STRING_REQ ="https://jsonplaceholder.typicode.com/users/1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +43,7 @@ public class request_user_api extends AppCompatActivity {
         req.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                makeStringReq();
+                makeJsonObjReq();
             }
         });
 
@@ -75,6 +80,7 @@ public class request_user_api extends AppCompatActivity {
                         // Handle any errors that occur during the request
                         res.setText("Error");
                         Log.e("Volley Error", error.toString());
+                        System.out.println(error);
                     }
                 }
         ) {
@@ -97,5 +103,47 @@ public class request_user_api extends AppCompatActivity {
 
         // Adding request to request queue
         VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+    }
+
+    private void makeJsonObjReq() {
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.GET,
+                URL_STRING_REQ,
+                null, // Pass null as the request body since it's a GET request
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Volley Response", response.toString());
+                        res.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        res.setText("Error");
+
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
     }
 }
