3980eae9de265b450b105a97f7e502bba11030b5
Nhat Bui <nbui@iastate.edu>
2023-10-09 08:32:41 -0500

    merge step 2

--- Frontend/app/src/main/java/com/kewargs/cs309/activity/dashboard/DashboardActivity.java
+++ Frontend/app/src/main/java/com/kewargs/cs309/activity/dashboard/DashboardActivity.java
@@ -2,7 +2,7 @@ package com.kewargs.cs309.activity.dashboard;
 
 import android.os.Bundle;
 import android.widget.TextView;
-
+import org.json.*;
 import com.kewargs.cs309.R;
 import com.kewargs.cs309.core.activity.AbstractActivity;
 import com.kewargs.cs309.utils.backend.factory.UserRequestFactory;
@@ -12,13 +12,28 @@ public class DashboardActivity extends AbstractActivity {
 
     private TextView userInfoDump;
 
+    private TextView dashboardGreeting;
+    JSONObject userInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         session.addRequest(UserRequestFactory
             .getUserById(session.getUserId())
-            .onResponse(response -> userInfoDump.setText(response))
+            .onResponse(response -> {
+                userInfoDump.setText(response);
+                try {
+                    userInfo = new JSONObject(response);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    dashboardGreeting.setText("Hello "+ userInfo.getString("display_name"));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            })
             .build()
         );
     }
@@ -26,5 +41,8 @@ public class DashboardActivity extends AbstractActivity {
     @Override
     protected void collectElements() {
         userInfoDump = findViewById(R.id.userInfoDump);
+        dashboardGreeting = findViewById(R.id.dashboardGreeting);
     }
+
+
 }
