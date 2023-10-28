1c7a28be1044e6ef4c7c2d9e6ec6c83ebdc827c9
Khoi <duckhoi311@gmail.com>
2023-10-28 14:19:53 -0500

    fixReturnFormat

--- Backend/src/main/java/cs309/backend/controllers/DegreeAuditController.java
+++ Backend/src/main/java/cs309/backend/controllers/DegreeAuditController.java
@@ -2,12 +2,16 @@ package cs309.backend.controllers;
 
 import cs309.backend.services.ReaderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedHashSet;
+
+import static org.springframework.http.ResponseEntity.internalServerError;
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +20,13 @@ public class DegreeAuditController {
     private final ReaderService readerService;
 
     @PostMapping("/pdf")
-    public LinkedHashSet<String> readingDegreeAudit(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<LinkedHashSet<String>> readingDegreeAudit(@RequestParam("file") MultipartFile file){
         try (InputStream inputStream = file.getInputStream()) {
-            return readerService.extractTextFromPdf(inputStream);
+            return ok(readerService.extractTextFromPdf(inputStream));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return internalServerError().build();
         }
     }
 }
