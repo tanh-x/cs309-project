02582a26427f5ae9fbd094b42f82708e1ecfada6
Kim Huynh <step1994@istate.edu>
2023-10-31 15:36:06 -0500

    sync

--- Backend/src/main/java/cs309/backend/auth/ApplicationConfig.java
+++ Backend/src/main/java/cs309/backend/auth/ApplicationConfig.java
@@ -1,20 +1,16 @@
 package cs309.backend.auth;
 
-import cs309.backend.jpa.entity.user.UserEntity;
-import cs309.backend.jpa.repo.UserRepository;
 import cs309.backend.services.UserService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 @Configuration
 @RequiredArgsConstructor
 
