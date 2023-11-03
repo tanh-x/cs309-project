af6379f59d0e36f23d66e32fff612705fdf54a7b
Thanh <kewber0@gmail.com>
2023-11-02 23:41:49 -0500

    Backend API endpoints for serving new Section-Schedule relationship. Also includes a working bidirectional reference between entities

---
+++ Backend/src/main/java/cs309/backend/jpa/entity/ScheduleEntity.java
@@ -0,0 +1,40 @@
+package cs309.backend.jpa.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Schedule")
+public class ScheduleEntity {
+    @Id
+    @Column(name = "id")
+    private int id;
+
+//    @Column(name = "section_id")
+//    private int sectionId;
+
+    @Column(name = "start_time")
+    private Integer startTime;
+
+    @Column(name = "end_time")
+    private Integer endTime;
+
+    @Column(name = "meet_days_bitmask")
+    private Integer meetDaysBitmask;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "instructor")
+    private String instructor;
+
+    @Column(name = "instruction_type")
+    private String instructionType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "section_id", referencedColumnName = "id")
+    @JsonBackReference  // Very VERRRYYYY important
+    private SectionEntity section;
+}
--- Backend/src/main/java/cs309/backend/jpa/entity/SectionEntity.java
+++ Backend/src/main/java/cs309/backend/jpa/entity/SectionEntity.java
@@ -1,42 +1,45 @@
 package cs309.backend.jpa.entity;
 
-import cs309.backend.jpa.entity.user.SectionEntityId;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Data
-@Table(name = "Schedule")
-@IdClass(SectionEntityId.class)
+@Table(name = "Section")
 public class SectionEntity {
     @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "ref_num")
+    private int refNum;
+
     @Column(name = "course_id")
     private int courseId;
 
-    @Id
     @Column(name = "section")
     private String section;
 
-    @Id
     @Column(name = "year")
     private int year;
 
-    @Id
     @Column(name = "season")
     private int season;
 
     @Column(name = "is_online")
     private Boolean isOnline;
 
-    @Column(name = "start_time")
-    private Integer startTime;
-
-    @Column(name = "end_time")
-    private Integer endTime;
-
-    @Column(name = "location")
-    private String location;
-
-    @Column(name = "instructor")
-    private String instructor;
+    @OneToMany(
+        mappedBy = "section",
+        targetEntity = ScheduleEntity.class,
+        cascade = CascadeType.ALL,
+        orphanRemoval = true,
+        fetch = FetchType.EAGER  // Important
+    )
+    @JsonManagedReference  // VERY important
+    private List<ScheduleEntity> schedules;
 }
