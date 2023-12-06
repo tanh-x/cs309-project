3f917f76d666e9be688c30d19d72443f5db729a8
Peachie-Codinson <buinhatanh309@gmail.com>
2023-12-06 11:49:36 -0600

    the ui

---
+++ Frontend/app/src/main/java/com/kewargs/cs309/core/utils/CourseHelper.java
@@ -0,0 +1,11 @@
+package com.kewargs.cs309.core.utils;
+
+public class CourseHelper {
+    public Course c;
+    public Schedule s;
+
+    public CourseHelper(Course c, Schedule s) {
+        this.c = c;
+        this.s = s;
+    }
+}
