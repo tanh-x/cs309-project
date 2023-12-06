97036eb202caf6ec0f3df2091d2cc22ff4519df5
Khoi <duckhoi311@gmail.com>
2023-12-06 12:47:18 -0600

    j

---
+++ Backend/src/test/java/CourseTest.java
@@ -0,0 +1,46 @@
+import cs309.backend.BackendApplication;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = BackendApplication.class)
+public class CourseTest {
+    String jwtToken = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6ImFkbWluIiwiaWF0IjoxNjk4NzI0NjIwLCJleHAiOjE3MzAyNjA2MjB9.gm4PTI169mmfAndcZQKFPwo3Ctl247a7gG-GQkIhw_LZoRVcgS9-8Z76sRNfcZXs";
+    @LocalServerPort
+    int port;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+        RestAssured.baseURI = "http://localhost";
+    }
+    @Test
+    public void getAllCourses() {
+        Response response = RestAssured.given().
+                header("Authorization", "Bearer " + jwtToken).
+                pathParams("term", 1).
+                get("/api/course/all/{term}");
+
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+    }
+
+    @Test
+    public void getCourseById() {
+        Response response = RestAssured.given().
+                header("Authorization", "Bearer " + jwtToken).
+                pathParams("id", 2).
+                get("/api/course/{id}");
+
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+    }
+}
--- Backend/src/test/java/UserTest.java
+++ Backend/src/test/java/UserTest.java
@@ -5,7 +5,6 @@ import cs309.backend.jpa.entity.user.StudentEntity;
 import cs309.backend.jpa.entity.user.UserEntity;
 import cs309.backend.jpa.repo.*;
 import cs309.backend.services.UserService;
-import jakarta.persistence.EntityNotFoundException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,8 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
@@ -91,4 +92,72 @@ public class UserTest {
         }
         verify(userRepository, times(3)).save(any(UserEntity.class));
     }
+    @Test
+    public void testGrantPermission() {
+        // Mock user
+        UserEntity mockUser = new UserEntity();
+        mockUser.setPrivilegeLevel(1);
+        mockUser.setUid(1);
+
+        // Mock getUserByUid method
+        when(userRepository.getUserByUid(anyInt())).thenReturn(mockUser);
+
+        // Mock countByUid method
+        when(studentProgramRepository.countByUid(anyInt())).thenReturn(0);
+
+        // Mock delete methods
+        doNothing().when(studentRepository).delete(any());
+        doNothing().when(studentProgramRepository).delete(any());
+
+        String result = userService.grantPermission(1, 1);
+        assertEquals(result, "Not a correct privilege");
+        // Call the method to test
+        result = userService.grantPermission(1, 2);
+
+        // Verify interactions
+        verify(userRepository, times(1)).getUserByUid(1);
+        verify(studentProgramRepository, times(1)).countByUid(1);
+        verify(studentRepository, times(1)).delete(any());
+        verify(staffRepository, times(2)).save(any(StaffEntity.class));//1 more in register
+        verify(adminRepository, times(1)).save(any(AdminEntity.class));//1 more in register
+
+        // Assertions
+        assertEquals("Successful", result);
+        assertEquals(2, mockUser.getPrivilegeLevel());
+
+        result = userService.grantPermission(1, 2);
+        assertEquals("This is you current privilege", result);
+
+        result = userService.grantPermission(1, 3);
+        verify(userRepository, times(3)).getUserByUid(1);
+        verify(studentProgramRepository, times(2)).countByUid(1);
+        verify(studentRepository, times(2)).delete(any());
+        verify(staffRepository, times(2)).save(any(StaffEntity.class));//1 more in register
+        verify(adminRepository, times(2)).save(any(AdminEntity.class));//1 more in register
+        assertEquals("Successful", result);
+        assertEquals(3, mockUser.getPrivilegeLevel());
+    }
+
+    @Test
+    public void testUpdateUser() {
+        int uid = 10;
+        String email = "newemail@example.com";
+        String displayName = "NewDisplayName";
+
+        UserEntity userEntity = new UserEntity("existingUser", "existingemail@example.com", "ExistingDisplayName", 1, "hashedpassword");
+        userEntity.setUid(uid);
+
+
+        when(userRepository.getUserByUid(uid)).thenReturn(userEntity);
+
+        boolean result = userService.updateUser(uid, email, displayName);
+
+        assertTrue(result);
+        verify(userRepository, times(1)).save(userEntity);
+
+        assertEquals(email, userEntity.getEmail());
+        assertEquals(displayName, userEntity.getDisplayName());
+    }
+
+
 }
